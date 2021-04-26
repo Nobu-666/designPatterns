6c16531fda2d3ed8f8432806b6d8bd5d5590d12a
86156 <2636821537@qq.com>
2021-04-26 22:01:05 +0800

    设计模式

---
+++ Client/Hub.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class Hub implements NetworkDevice
+{
+    private String type;
+    public Hub(String type)
+    {
+        this.type=type;
+    }
+    public String getType()
+    {
+        return this.type;
+    }
+    public void use()
+    {
+        System.out.println("Linked by Hub,type is"+this.type);
+    }
+}
---
+++ Client/NetworkDevice.java
@@ -0,0 +1,7 @@
+package Test;
+
+public interface NetworkDevice
+{
+    public String getType();
+    public void use();
+}
